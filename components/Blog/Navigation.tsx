59da7d5c34494a1d8e42e5bf4e868e603ffb4032
Daniel Kluge <daniel-git@c0ntroller.de>
2022-10-03 01:19:43 +0200

    More Navigation work

--- components/Blog/Navigation.tsx
+++ components/Blog/Navigation.tsx
@@ -1,10 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
+import Link from "next/link";
+import styles from "../../styles/Blog/Navigation.module.scss";
 
 const Navigation: NextPage<{}> = () => {
-    return <nav>
-        <div className="logo"></div>
-        <div className="navLink">Projects</div>
-        <div className="navLink">About me</div>
+    return <nav className={styles.navigation}>
+        <Link href={"/"} passHref>
+            <picture>
+                <source srcSet="/icon.png" type="image/png" />
+                <img src="/icon.png" alt={"Website icon, a red eye"} className={styles.logo} />
+            </picture>
+        </Link>
+        <div className={styles.navLink}>Projects</div>
+        <div className={styles.navLink}>About me</div>
     </nav>;
 };
 
