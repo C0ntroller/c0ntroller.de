880af043a940aedf02a4ceee0d75327b9e0cc249
Daniel Kluge <daniel-git@c0ntroller.de>
2022-10-18 23:40:27 +0200

    First bit about me

---
+++ pages/me.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next";
+import Layout from "../components/Blog/Layout";
+
+import styles from "../styles/Blog/AboutMe.module.scss";
+
+const SkillBar: NextPage<{ skill: string, color: string, pct: number }> = ({skill, color, pct}) => {
+    return <></>;
+};
+
+const AboutMe: NextPage = () => {
+    const age = new Date().getFullYear() - 1998 - (new Date().getMonth() < 10 ? 1 : 0);
+
+    return <Layout>
+        <h1>This is me.</h1>
+        <p className={styles.preText}>
+            Hi! My name is <strong>Daniel</strong> and I'm an automation engineer from Germany.
+        </p>
+        <p>
+            I'm currently studying <strong>Information Systems Engineering</strong> at the <strong>TU Dresden</strong><br/>
+            Currently I'm {age} years old.
+        </p>
+    </Layout>;
+
+};
+
+export default AboutMe;
