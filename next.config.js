4fe5f4a4448ea2d7dffe8a082005be5a1dabd36a
Daniel Kluge <daniel-git@c0ntroller.de>
2022-10-20 23:18:14 +0200

    I finally got it!!

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ module.exports = {
   images: {
     domains: ["openai-labs-public-images-prod.azureedge.net"]
   },
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push({
       test: /\.adoc$/i,
