dafbda9509461bebecd0bc40d16a02b75ea868b1
Daniel Kluge <daniel-git@c0ntroller.de>
2024-03-03 15:18:06 +0100

    Upgrade astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,23 @@
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
 import a11yEmoji from "@fec/remark-a11y-emoji";
 import wasmPack from 'vite-plugin-wasm-pack';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
     prefetchAll: true
   },
-  integrations: [mdx()],
+  integrations: [mdx(), icon()],
   markdown: {
-    remarkPlugins: [
-      a11yEmoji
-    ],
+    remarkPlugins: [a11yEmoji],
     shikiConfig: {
       theme: "one-dark-pro"
     }
   },
   // Copy wasm-terminal to the build directory
   vite: {
-    plugins: [
-      wasmPack([],['@c0ntroller/wasm-terminal'])
-    ]
+    plugins: [wasmPack([], ['@c0ntroller/wasm-terminal'])]
   }
 });
